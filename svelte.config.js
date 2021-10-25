48d7ceead06ef161a15a028499b041e2da836390
j <j@pop-os.localdomain>
2021-10-25 14:37:00 -0400

    gitignore

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
 import preprocess from 'svelte-preprocess';
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	adapter: static_adapter(),
+
+	paths: {
+		base: '/mcrypt',
+		assets: '/mcrypt'
+	},
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallbadk: null
+		})
 	}
 };
 
