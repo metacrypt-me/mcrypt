6090105fee171dc59ab59e0e6077fae9413fe57b
j <j@pop-os.localdomain>
2021-10-25 16:24:40 -0400

    Basic deploy to GH Pages working

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>MCRP</h1>
+	<h3>MetaCrypt Cardano Stake Pool</h3>
 </main>
 
 <style>
